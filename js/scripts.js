393d5aec04e2df4df5524a0cf47e19154b25a238
Chance Neff and Martin Cartledge <martin@>
2016-08-17 09:01:05 -0700

    refactor if else statments for divisible by 4 and 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 // Back-End
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
-  } else {
+} else {
     return false;
   }
 };
